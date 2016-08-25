572bc3064d3d648091e954538f832b2019f67e85
Shuaibird <shuaibird@icloud.com>
2016-08-25 08:25:56 +0800

    connect controller with router to access the model

--- server/api/category/categoryRoutes.js
+++ server/api/category/categoryRoutes.js
@@ -1,10 +1,16 @@
 var router = require('express').Router()
 var logger = require('../../util/logger')
+var controller = require('./categoryController')
+
+router.param('id', controller.params)
 
 router.route('/')
-  .get((req, res) => {
-    logger.log('Hey from category!!')
-    res.send({ok: true})
-  })
+  .get(controller.get)
+  .post(controller.post)
+
+router.route('/:id')
+  .get(controller.getOne)
+  .put(controller.put)
+  .delete(controller.delete)
 
 module.exports = router
