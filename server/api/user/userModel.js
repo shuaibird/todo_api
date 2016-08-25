2151c24e43aa19c12c70ee006ede785bee2903f1
Shuaibird <shuaibird@icloud.com>
2016-08-25 11:14:10 +0800

    add authentication for user

--- server/api/user/userModel.js
+++ server/api/user/userModel.js
@@ -1,12 +1,38 @@
 var mongoose = require('mongoose')
+var bcrypt = require('bcrypt')
 
 var userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
     unique: true
+  },
+  password: {
+    type: String,
+    required: true
   }
 })
+
+userSchema.pre('save', function(next) {
+  if (!this.isModified('password'))
+    return next()
+  this.password = this.encryptPassword(this.password)
+  next()
+})
+
+useSchema.methods = {
+  authenticate: function(plainTextPassword) {
+    return bcrypt.compareSync(plainTextPassword, this.password)
+  },
+  encryptPassword: function(plainTextPassword) {
+    if (!plainTextPassword) {
+      return ''
+    } else {
+      var salt = bcrypt.genSaltSync(10)
+      return bcrypt.hashSync(plainTextPassword, salt)
+    }
+  }
+}
 
 var userModel = mongoose.model('user', userSchema)
 
